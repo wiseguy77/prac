44ac88d74844f512333e01e4a2b67fae12eea485
mhhyun <ultrawise77@gmail.com>
2025-04-16 22:58:05 +0900

    feat: 동적비교 검색조건으로 멤버 조회 기능 추가
    - 동적비교 검색조건 시 Filter DTO 사용

--- src/main/java/wise/study/prac/mvc/controller/api/MemberController.java
+++ src/main/java/wise/study/prac/mvc/controller/api/MemberController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import wise.study.prac.mvc.dto.MemberAllResponse;
+import wise.study.prac.mvc.dto.MemberFilterRequest;
+import wise.study.prac.mvc.dto.MemberListResponse;
 import wise.study.prac.mvc.dto.MemberRequest;
 import wise.study.prac.mvc.dto.MemberResponse;
 import wise.study.prac.mvc.dto.MemberTeamResponse;
@@ -60,17 +62,17 @@ public class MemberController {
   @GetMapping("/me/with-team")
   public MemberResponse getMeWithTeam(@AuthenticationPrincipal JwtUserDetails user) {
 
-    return memberService.getMemberTeamInfo(MemberRequest.builder()
+    return memberService.getMemberTeam(MemberRequest.builder()
         .account(user.getAccount()).build());
   }
 
   @Operation(summary = "모든 사용자 정보 조회", description = "모든 사용자 정보를 조회합니다.")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "모든 사용자 정보 조회 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MemberAllResponse.class))),
+      @ApiResponse(responseCode = "200", description = "모든 사용자 정보 조회 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MemberListResponse.class))),
       @ApiResponse(responseCode = "500", description = "모든 사용자 정보 조회 실패", content = @Content(mediaType = "application/json", schema = @Schema()))
   })
   @GetMapping()
-  public MemberAllResponse getAllMember() {
+  public MemberListResponse getAllMember() {
 
     return memberService.getAllMemberInfo();
   }
@@ -81,7 +83,7 @@ public class MemberController {
       @ApiResponse(responseCode = "500", description = "모든 사용자 정보 조회 실패", content = @Content(mediaType = "application/json", schema = @Schema()))
   })
   @GetMapping("/{id}")
-  public MemberResponse getMemberById(@PathVariable int id) {
+  public MemberResponse getMemberById(@PathVariable long id) {
 
     return memberService.getMemberById(id);
   }
@@ -103,8 +105,19 @@ public class MemberController {
       @ApiResponse(responseCode = "500", description = "복합 조건으로 사용자와 팀 정보 조회 실패", content = @Content(mediaType = "application/json", schema = @Schema()))
   })
   @PostMapping("/with-team/search")
-  public MemberTeamResponse getMemberTeam(@RequestBody MemberRequest request) {
+  public List<MemberTeamResponse> getMemberTeam(@RequestBody MemberRequest request) {
 
-    return memberService.getMemberTeamInfo(request);
+    return memberService.getMemberTeamList(request);
+  }
+
+  @Operation(summary = "동적 조건으로 사용자와 팀 정보 조회", description = "동적 조건으로 사용자와 팀 정보를 함께 조회합니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "동적 조건으로 사용자와 팀 정보 조회 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MemberTeamResponse.class))),
+      @ApiResponse(responseCode = "500", description = "동적 조건으로 사용자와 팀 정보 조회 실패", content = @Content(mediaType = "application/json", schema = @Schema()))
+  })
+  @PostMapping("/filter")
+  public MemberListResponse filterMember(@RequestBody MemberFilterRequest request) {
+
+    return memberService.filterMemberList(request);
   }
 }
