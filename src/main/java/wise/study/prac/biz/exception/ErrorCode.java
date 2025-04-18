d884a51e5d67bd957aa24d0f1f7c0bdba32557a4
mhhyun <ultrawise77@gmail.com>
2025-04-19 00:54:44 +0900

    refact: 예외 코드 추가 및 분리

--- src/main/java/wise/study/prac/biz/exception/ErrorCode.java
+++ src/main/java/wise/study/prac/biz/exception/ErrorCode.java
@@ -5,6 +5,9 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
+  /**
+   * 500
+   **/
   REGISTER_MEMBER_FAIL("REGISTER_MEMBER_FAIL", "사용자 가입에 실패햇습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
   KEY_GENERATION_FAIL("KEY_GENERATION_FAIL", "개인 서명 키 생성에 실패했습니다.",
       HttpStatus.INTERNAL_SERVER_ERROR),
@@ -18,11 +21,17 @@ public enum ErrorCode {
   UNAUTHORIZED("UNAUTHORIZED", "인증에 실패했습니다.", HttpStatus.UNAUTHORIZED),
   ACCESS_DENIED("ACCESS_DENIED", "접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
   JSON_CONVERT("JSON_CONVERT_ERROR", "JSON (역)직렬화에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-  ILLEGAL_ARGUMENTS("ILLEGAL_ARGUMENTS", "잘못된 요청 파라미터 보냈습니다.", HttpStatus.BAD_REQUEST),
+  INTERNAL_SERVER_ERROR("INTERNAL_ERROR", "서버 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
   READ_REQUEST_BODY_FAIL("READ_REQUEST_BODY_FAIL", "사용자 요청 객체 정보를 얻는데 실패했습니다.",
       HttpStatus.INTERNAL_SERVER_ERROR),
+
+  /**
+   * 400
+   **/
+  ILLEGAL_ARGUMENTS("ILLEGAL_ARGUMENTS", "잘못된 요청 파라미터 보냈습니다.", HttpStatus.BAD_REQUEST),
   NO_RESOURCE_FOUND("NO_RESOURCE_FOUND", "요청한 경로를 찾을 수 없습니다", HttpStatus.NOT_FOUND),
-  INTERNAL_SERVER_ERROR("INTERNAL_ERROR", "서버 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+  INVALID_REQUEST_BODY("INVALID_REQUEST_BODY", "요청 본문이 잘못되었습니다.", HttpStatus.BAD_REQUEST),
+  BAD_REQUEST("BAD_REQUEST", "잘못된 요청입니다.", HttpStatus.BAD_REQUEST);
 
   private final String code;
   private final String message;
