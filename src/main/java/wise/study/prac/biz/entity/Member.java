e517f386fc47b8ab57f95bdf66caca50b88d1b05
mhhyun <ultrawise77@gmail.com>
2025-04-20 20:49:41 +0900

    refact: Member 연관엔티티 Team 지연로딩

--- src/main/java/wise/study/prac/biz/entity/Member.java
+++ src/main/java/wise/study/prac/biz/entity/Member.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -63,7 +64,7 @@ public class Member extends BaseEntity implements JwtInfo {
   @Enumerated(EnumType.STRING)
   private RoleType role = RoleType.USER;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "team_id")
   private Team team;
 
