0f5f5778548ecadbd6c433233907b6d7cd6bbc8d
mhhyun <ultrawise77@gmail.com>
2025-04-29 19:50:55 +0900

    refact: EmptyResponse Json 애노테이션 추가

--- src/main/java/wise/study/prac/biz/dto/EmptyResponse.java
+++ src/main/java/wise/study/prac/biz/dto/EmptyResponse.java
@@ -1,10 +1,11 @@
 package wise.study.prac.biz.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EmptyResponse {
-
 }
